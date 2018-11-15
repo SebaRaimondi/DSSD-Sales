5735d61178dc478208d299e1ab3aad396f014097
Sebastian Raimondi <sebastianraimondi@gmail.com>
2018-11-15 19:17:59 -0300

    Add Models and Migrations

---
+++ migrations/20181115221253-create-sale.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Sales', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      productid: {
+        type: Sequelize.INTEGER
+      },
+      quantity: {
+        type: Sequelize.INTEGER
+      },
+      date: {
+        type: Sequelize.DATE
+      },
+      price: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Sales');
+  }
+};
---
+++ models/sale.js
@@ -0,0 +1,13 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Sale = sequelize.define('Sale', {
+    productid: DataTypes.INTEGER,
+    quantity: DataTypes.INTEGER,
+    date: DataTypes.DATE,
+    price: DataTypes.INTEGER
+  }, {});
+  Sale.associate = function(models) {
+    // associations can be defined here
+  };
+  return Sale;
+};
